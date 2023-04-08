0064bcae22a90630ff440657b728a67682336b88
Leonid Kuznetsov <kuznetsovlv1981@gmail.com>
2023-04-08 23:49:43 +0300

    Restored Item's code

--- src/pages/Gateways/Item.jsx
+++ src/pages/Gateways/Item.jsx
@@ -1,1 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import PropTypes from 'prop-types';
+import { DeleteButton, Table } from 'components';
+import { GATEWAY_ROUTE } from 'routes';
+
+const Item = ({ serial, name, onClick }) => (
+  <Table.Row
+    key={serial}
+    link={`${GATEWAY_ROUTE}?serial=${serial}`}
+    title={`Gateway ${name}/${serial}`}
+  >
+    <Table.Cell>{name}</Table.Cell>
+    <Table.Cell>{serial}</Table.Cell>
+    <Table.Cell align="right">
+      <DeleteButton
+        onClick={useCallback(() => onClick(serial), [serial, onClick])}
+      />
+    </Table.Cell>
+  </Table.Row>
+);
+
+Item.propTypes = {
+  serial: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+export default Item;
